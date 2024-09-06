1e71dcacbf7c583868afa99195b705168fba8d60
mannuelst <manuelds@outlook.pt>
2024-09-06 20:05:51 +0100

    feat(schedule): add notification-task-user

--- task-manager/src/infra/jobs/notification-task-user.schedule.ts
+++ task-manager/src/infra/jobs/notification-task-user.schedule.ts
@@ -3,8 +3,12 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class NotificationTaskUserSchedule {
-  @Cron(CronExpression.EVERY_5_SECONDS)
+  @Cron(CronExpression.EVERY_5_SECONDS) // definindo tempo que essa cron irá executar!
   getAllTasksDay() {
     console.log('Task OK!', new Date());
   }
 }
+
+/**
+ * jobs: são crons que serão executadas
+ */
